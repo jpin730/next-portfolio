ef947feba9796e0f7b8c1259c2223b91ff78b8c3
Jaime Pineda <jpin730@pm.me>
2026-04-10 16:05:19 -0600

    feat: :sparkles: add NotFound component for 404 error page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+export default function NotFound() {
+  return (
+    <main>
+      <div className="flex h-dvh w-dvw items-center justify-center gap-4">
+        <p className="text-4xl font-bold">404</p>
+        <span className="block h-16 w-0.5 bg-black dark:bg-white"></span>
+        <h1 className="text-2xl">Page Not Found</h1>
+      </div>
+    </main>
+  )
+}
