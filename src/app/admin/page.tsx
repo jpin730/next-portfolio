b15d088c0a383d948025757ca664ce8a1c5c413d
Jaime Pineda <jpin730@pm.me>
2026-04-10 18:45:37 -0600

    feat: :passport_control: add Auth0 authentication for admin page and proxy middleware

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,22 @@
+import { auth0 } from '@/auth/lib/auth0'
+
+export default async function Page() {
+  const session = await auth0.getSession()
+
+  if (!session) {
+    return <a href="/auth/login">Login</a>
+  }
+
+  return (
+    <>
+      <p>Logged in as {session.user.email}</p>
+
+      {/* Display user info (name, email, etc.) */}
+      <h1>User Profile</h1>
+      <pre>{JSON.stringify(session.user, null, 2)}</pre>
+
+      {/* Ends the session and redirects to Auth0 to log out */}
+      <a href="/auth/logout">Logout</a>
+    </>
+  )
+}
